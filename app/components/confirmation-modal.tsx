1903d9893613a7bf767b1cdc624e4d576bac663d
jeremiahfernandez97 <jeremiahfernandez97@gmail.com>
2024-05-29 14:10:31 +0800

    added sign out button

---
+++ app/components/confirmation-modal.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogOverlay,
+    Button,
+} from '@chakra-ui/react'
+
+export default function ConfirmationModal({
+    isOpen,
+    onClose,
+    cancelRef,
+    onConfirm,
+    action,
+    message,
+}: any) {
+    return (
+        <>
+            <AlertDialog
+                isOpen={isOpen}
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                            {action}
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>{message}</AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button ref={cancelRef} onClick={onClose}>
+                                Cancel
+                            </Button>
+                            <Button
+                                colorScheme="red"
+                                onClick={() => {
+                                    onConfirm()
+                                    onClose()
+                                }}
+                                ml={3}
+                            >
+                                {action}
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </>
+    )
+}
--- app/link-generator/page.tsx
+++ app/link-generator/page.tsx
@@ -1,27 +1,31 @@
 'use client'
 
-import { useCallback, useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { auth, db } from '../firebase/firebase'
 import {
   Box,
   Container,
   Center,
-  FormErrorMessage,
-  FormControl,
-  FormLabel,
-  Input,
   Button,
   Heading,
   useToast,
 } from '@chakra-ui/react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { collection, addDoc } from 'firebase/firestore'
+import ConfirmationModal from '../components/confirmation-modal'
+import { useDisclosure } from '@chakra-ui/react'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useRouter } from 'next/navigation'
 
 export default function LinkGenerator() {
   const [loading, setLoading] = useState(false)
   const [generatedUID, setGeneratedUID] = useState("")
   const [user, error] = useAuthState(auth)
   const toast = useToast()
+  const router = useRouter();
+  const [signoutMessage, setSignoutMessage] = useState('Are you sure you want to sign out of ' + user?.email + '?')
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
 
   const handleCreate = (e: any) => {
     e.preventDefault()
@@ -51,23 +55,71 @@ export default function LinkGenerator() {
         setLoading(false)
       })
   }
+
+  const handleSignOut = useCallback(async () => {
+    try {
+        await auth.signOut()
+        toast({
+            title: 'Success!',
+            description: 'Signed out',
+            status: 'success',
+            duration: 9000,
+            isClosable: true,
+        })
+    } catch (e) {
+      toast({
+          title: 'Internal Error',
+          description: e + "",
+          status: 'error',
+          duration: 9000,
+          isClosable: true,
+      })
+    }
+  }, [toast, auth])
+
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+        router.push('/login')
+    }
+  })
+
   return (
-    <Center h="90vh">
-      <Container textAlign="center">
-          <Box>
-            <Heading mb="10">Welcome, <span style={{color:"#2F855A"}}>{user?.email}</span></Heading>
-          </Box>
-          <Box>
-            <form onSubmit={handleCreate}>
-              <Button mb="10" type="submit" isDisabled={loading}>
-                  Create
-              </Button>
-            </form>
-          </Box>
-          <Box>
-            Generated UID: {generatedUID}
-          </Box>
-      </Container>
-    </Center>
+    <>
+      <Center h="90vh">
+        <Container textAlign="center">
+            <Box>
+              <Heading mb="3">Welcome, <span style={{color:"#2F855A"}}>{user?.email}</span></Heading>
+            </Box>
+                  <Button
+                      mb="10"
+                      variant="link"
+                      onClick={() => {
+                          onOpen()
+                      }}
+                  >
+                      Sign out
+                  </Button>
+            <Box>
+              <form onSubmit={handleCreate}>
+                <Button mb="10" type="submit" isDisabled={loading}>
+                    Create
+                </Button>
+              </form>
+            </Box>
+            <Box>
+              Generated UID: {generatedUID}
+            </Box>
+        </Container>
+      </Center>
+
+      <ConfirmationModal
+        isOpen={isOpen}
+        onClose={onClose}
+        cancelRef={cancelRef}
+        onConfirm={() => handleSignOut()}
+        action="Sign out"
+        message={signoutMessage}
+      />
+    </>
   )
 }
